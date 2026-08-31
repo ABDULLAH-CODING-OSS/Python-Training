14128e3cd38a99a6f818d5cd2747ff17669bc9e7
ABDULLAH JAVED <jatt66016@gmail.com>
2026-08-31 16:49:17 +0500

    Modifications

--- Web-Scrapper/main.py
+++ Web-Scrapper/main.py
@@ -10,6 +10,9 @@ url = base_url
 all_books=[]
 
 csv_filename = "books.csv"
+session = requests.Session()
+book_count =0
+headers = {"User-Agent": "Mozilla/5.0 (compatible; MyScraperBot/1.0)"}
 
 
 try:
@@ -20,18 +23,23 @@ try:
 
         while url:
             print(f"Scraping page: {url}")
-            response = requests.get(url)
+            session.headers.update({"User-Agent": "Mozilla/5.0 (compatible; MyScraperBot/1.0)"})
+            response = session.get(url, timeout=10, headers=headers)
+            response.raise_for_status()
             soup = BeautifulSoup(response.content, "html.parser")
             books = soup.find_all("article", class_="product_pod")
 
             for book in books:
                 title = book.h3.a["title"]
                 price = book.find("p", class_="price_color").text
-                rating = book.find("p", class_="star-rating")["class"][1]
+                rating_tag= book.find("p", class_="star-rating")
+                rating = rating_tag["class"][1] if rating_tag and len(rating_tag["class"])>1 else "Unknown"
                 print(f"Title: {title}, Price: {price}, Rating: {rating}")
                 book_data = {"title": title, "price": price, "rating": rating}
                 all_books.append(book_data)
                 writer.writerow(book_data)
+                book_count += 1
+                print(f"Book count: {book_count}")
 
             next_button = soup.find("li", class_="next")
             if next_button:
