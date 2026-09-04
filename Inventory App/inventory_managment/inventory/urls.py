3a3a66fc28dbc2f431d4ecc8fbf0b3c142c70168
ABDULLAH JAVED <jatt66016@gmail.com>
2026-09-04 15:54:39 +0500

    Login Page Introduced

--- Inventory App/inventory_managment/inventory/urls.py
+++ Inventory App/inventory_managment/inventory/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import Index, SignUpView
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns= [
     path('', Index.as_view(), name='index'),
     path('Signup/', SignUpView.as_view(), name='signup'),
+    path('Login/', auth_views.LoginView.as_view(template_name='inventory/login.html'), name= 'login')
 
 ]
--- Inventory App/inventory_managment/inventory_managment/settings.py
+++ Inventory App/inventory_managment/inventory_managment/settings.py
@@ -130,3 +130,6 @@ MAILERS = {
 }
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+
+LOGIN_REDIRECT_URL = '/dashboard/'  # Redirect to the dashboard after successful login
