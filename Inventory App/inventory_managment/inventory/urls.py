a023ef208a842f17b9d1065b2af4f401898ab6ac
ABDULLAH JAVED <jatt66016@gmail.com>
2026-09-04 17:42:26 +0500

    Logout implemented with some enhancements

--- Inventory App/inventory_managment/inventory/urls.py
+++ Inventory App/inventory_managment/inventory/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns= [
     path('', Index.as_view(), name='index'),
-    path('Signup/', SignUpView.as_view(), name='signup'),
-    path('Login/', auth_views.LoginView.as_view(template_name='inventory/login.html'), name= 'login')
-
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='inventory/login.html'), name= 'login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='inventory/logout.html'), name='logout'),
 ]
