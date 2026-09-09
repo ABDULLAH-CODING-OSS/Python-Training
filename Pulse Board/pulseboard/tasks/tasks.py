231cdd401b052f5e4f571f03202d634a9289fc51
ABDULLAH JAVED <jatt66016@gmail.com>
2026-09-09 19:15:03 +0500

    Celery Beat Retry Logic Implemented

--- Pulse Board/pulseboard/tasks/tasks.py
+++ Pulse Board/pulseboard/tasks/tasks.py
@@ -2,11 +2,18 @@ import time
 from celery import shared_task
 
 @shared_task
-def send_task_assignment_notification(task_id, task_title, user_email=None):
-    print(f"[CELERY WORKER] Starting notification for Task ID: {task_id}")
-    time.sleep(2)
-    print(f"[CELERY WORKER] Notification sent for '{task_title}' to {user_email or 'Unassigned'}")
-    return f"Notification completed for task {task_id}"
+def send_task_assignment_notification(self, task_id, task_title, user_email=None):
+    try:
+        print(f"[CELERY WORKER] Starting notification for Task ID: {task_id}")
+        time.sleep(2)
+        print(f"[CELERY WORKER] Notification sent for '{task_title}' to {user_email or 'Unassigned'}")
+        return f"Notification completed for task {task_id}"
+    except Exception as exc:
+        print("Error Occcured Retrying in 5 seconds.....")
+        raise self.retry(exc=exc)
+    
+        
+    
 
 
 
