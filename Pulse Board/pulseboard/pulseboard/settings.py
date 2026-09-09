558bb9ce10a60c408e21a0cd2e2cf25e581bc01b
ABDULLAH JAVED <jatt66016@gmail.com>
2026-09-09 17:14:37 +0500

    Celery Setup

--- Pulse Board/pulseboard/pulseboard/settings.py
+++ Pulse Board/pulseboard/pulseboard/settings.py
@@ -126,3 +126,10 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+CELERY_BROKER_URL = 'redis://localhost:6373/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
