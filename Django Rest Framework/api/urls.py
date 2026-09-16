d1891401cb5496f9225bb913f57221d5d9284d11
ABDULLAH JAVED <jatt66016@gmail.com>
2026-09-16 15:26:47 +0500

    Put and Delete Implemented

--- Django Rest Framework/api/urls.py
+++ Django Rest Framework/api/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("students", views.studentsView),
     path("students/<int:pk>/", views.StudentDetailView),
     path("employees/", views.Employees.as_view()),
+    path("employees/<int:pk>/", views.EmployeeDetailView.as_view()),
 ]
