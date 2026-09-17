506e02504f6416df14cc4f887aec444c8acd9075
ABDULLAH JAVED <jatt66016@gmail.com>
2026-09-17 19:24:05 +0500

    ORdering Custom and Builtin

--- Django Rest Framework/employees/filters.py
+++ Django Rest Framework/employees/filters.py
@@ -5,8 +5,20 @@ from .models import Employee
 class EmployeeFilter(django_filters.FilterSet):
     designation = django_filters.CharFilter(field_name="emp_dept", lookup_expr="iexact")
     name = django_filters.CharFilter(field_name="emp_name", lookup_expr="icontains")
-    id = django_filters.RangeFilter(field_name="emp_id")
+    #  id = django_filters.RangeFilter(field_name="emp_id")
+
+    id_min = django_filters.CharFilter(
+        method="filter_by_id_range", label="FROM EMP Id  "
+    )
+    id_max = django_filters.CharFilter(method="filter_by_id_range", label="TO EMP Id  ")
 
     class Meta:
         model = Employee
-        fields = ["designation", "name", "id"]
+        fields = ["designation", "name", "id_min", "id_max"]
+
+    def filter_by_id_range(self, queryset, name, value):
+        if name == "id_min":
+            return queryset.filter(emp_id__gte=value)
+        elif name == "id_max":
+            return queryset.filter(emp_id__lte=value)
+        return queryset
